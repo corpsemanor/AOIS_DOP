5858478ef7d3a6ad2c0ce64db8011d9a01412b74
corpsemanor <93287617+corpsemanor@users.noreply.github.com>
2023-06-25 18:16:18 +0300

    Add files via upload

---
+++ AOIS_DOP.cpp
@@ -0,0 +1,90 @@
+﻿#include <iostream>
+
+struct Node {
+    int data;     
+    Node* next;   
+    Node* skip;   
+
+    Node(int value) : data(value), next(nullptr), skip(nullptr) {}
+};
+
+class SkipList {
+public:
+    SkipList() : head(nullptr) {}
+
+    void add(int value) {
+        Node* newNode = new Node(value);
+
+        if (!head) {
+            head = newNode;
+            return;
+        }
+
+        Node* current = head;
+        Node* prev = nullptr;
+
+        while (current && current->data < value) {
+            prev = current;
+            current = current->next;
+        }
+
+        newNode->next = current;
+        if (prev) {
+            prev->next = newNode;
+        }
+
+        if (prev && prev->skip && prev->skip->data < value) {
+            newNode->skip = prev->skip;
+            prev->skip = newNode;
+        }
+        else if (prev) {
+            prev->skip = newNode;
+        }
+    }
+
+    bool search(int value) {
+        Node* current = head;
+
+        while (current) {
+            if (current->data == value)
+                return true;
+
+            if (current->skip && current->skip->data <= value)
+                current = current->skip;
+            else
+                current = current->next;
+        }
+
+        return false;
+    }
+
+    void printList() {
+        Node* current = head;
+        while (current) {
+            std::cout << current->data << " -> ";
+            current = current->next;
+        }
+        std::cout << "nullptr" << std::endl;
+    }
+
+private:
+    Node* head;
+};
+
+int main() {
+    SkipList list;
+
+    list.add(3);
+    list.add(6);
+    list.add(1);
+    list.add(9);
+    list.add(2);
+
+    std::cout << "List: ";
+    list.printList();
+
+    std::cout << "Searching for 6: " << (list.search(6) ? "Found" : "Not found") << std::endl;
+    std::cout << "Searching for 5: " << (list.search(5) ? "Found" : "Not found") << std::endl;
+
+    return 0;
+}
